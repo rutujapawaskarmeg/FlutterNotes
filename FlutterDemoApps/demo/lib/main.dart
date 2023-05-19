9c1ace8f3e9620e5e3ba3524ceebf82fbae0e303
rutujapawaskarmeg <rutuja.pawaskar@meg-nxt.com>
2023-05-19 12:03:14 +0530

    demo app counter sample app

--- FlutterDemoApps/demo/lib/main.dart
+++ FlutterDemoApps/demo/lib/main.dart
@@ -1,20 +1,23 @@
 // import helper library from flutter
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 //define a main fuc to run when our app starts
 void main(){
 // create a new text widget to show some text on screen
-var app = MaterialApp(
-  home: Scaffold(
-    floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.add),
-      onPressed: () { 
-        print('Hi There! Welcome');
-      },),
-    appBar: AppBar(
-      title: Text('Lets see some images!'),
-    ),
-  ),
-);
+// var app = MaterialApp(
+//   home: Scaffold(
+//     floatingActionButton: FloatingActionButton(
+//       child: Icon(Icons.add),
+//       onPressed: () {
+//         print('Hi There! Welcome');
+//       },),
+//     appBar: AppBar(
+//       title: Text('Lets see some images!'),
+//     ),
+//   ),
+// );
 //take that widget and get it on the screen
-runApp(app);
+//runApp(app);
+runApp(App());
 }
+
--- FlutterDemoApps/demo/lib/src/app.dart
+++ FlutterDemoApps/demo/lib/src/app.dart
@@ -1,5 +1,36 @@
 //Import flutter helper library
 import 'package:flutter/material.dart';
 //create a class that will be custom widget this must extend the 'StatelessWidget' base class
+class App extends StatefulWidget{
+   createState() {
+   return AppState();
+  }
 
-//the widget that 'this' widget will show
+}
+
+class AppState extends State<App>{
+  int counter =0; //instant variable
+  //must define a 'build' method that return the widget that 'this' widget will show
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter +=1;
+            });
+            //print('Hi There! Welcome');
+          },),
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+      ),
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
